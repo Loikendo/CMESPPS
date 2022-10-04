16e5508da8b561432f0f5e4b641096fce2a220c1
Alberto <Alberto@MSI>
2022-10-04 16:29:22 +0200

    commit inicial

---
+++ src/tutorial/HolaMundo.java
@@ -0,0 +1,9 @@
+package tutorial;
+
+public class HolaMundo {
+	public static void main (String [] src) {
+		System.out.println("Hola CMEPPS");
+		System.out.println("Bienvenidos al curso");
+	}
+
+}
